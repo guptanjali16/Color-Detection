08dde4e0f406f11e63483dfb7b715e348e6f134a
Anjali gupta <63281911+guptanjali16@users.noreply.github.com>
2021-08-29 09:02:55 +0530

    Add files via upload

---
+++ color_detection.py
@@ -0,0 +1,78 @@
+import cv2
+import numpy as np
+import pandas as pd
+import argparse
+
+
+#Creating argument parser to take image path from command line
+ap = argparse.ArgumentParser()
+ap.add_argument('-i', '--image', required=True, help="colorpic.jpg")
+args = vars(ap.parse_args())
+img_path = args['image']
+
+
+#Reading the image with opencv
+#img = cv2.imread("C:\\Users\\anjali\\Pictures\\creativity\\Untitled.jpg")
+img = cv2.imread(img_path)
+
+#declaring global variables (are used later on)
+clicked = False
+r = g = b = xpos = ypos = 0
+
+#Reading csv file with pandas and giving names to each column
+index=["color","color_name","hex","R","G","B"]
+csv = pd.read_csv('colors.csv', names=index, header=None)
+
+#Calculate distance to get color name 
+#now we have the r,g and b values. Now, we need another function which will return us the color name from RGB values. To get the color name, we calculate a distance(d) which tells us how close we are to color and choose the one having minimum distance.
+def getColorName(R,G,B):
+    minimum = 10000
+    for i in range(len(csv)):
+        d = abs(R- int(csv.loc[i,"R"])) + abs(G- int(csv.loc[i,"G"]))+ abs(B- int(csv.loc[i,"B"]))
+        if(d<=minimum):
+            minimum = d
+            cname = csv.loc[i,"color_name"]
+    return cname
+
+#function to get x,y coordinates of mouse double click
+#It will calculate the rgb values of the pixel which we double click. The function parameters have the event name, (x,y) coordinates of the mouse position, etc. In the function, we check if the event is double-clicked then we calculate and set the r,g,b values along with x,y positions of the mouse.
+def draw_function(event, x,y,flags,param):
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        global b,g,r,xpos,ypos, clicked
+        clicked = True
+        xpos = x
+        ypos = y
+
+        b,g,r = img[y,x]
+        b = int(b)
+        g = int(g)
+        r = int(r)
+       
+cv2.namedWindow('image')
+cv2.setMouseCallback('image',draw_function)
+
+while(1):
+
+    cv2.imshow("image",img)
+    if (clicked):
+   
+        #cv2.rectangle(image, startpoint, endpoint, color, thickness)-1 fills entire rectangle 
+        cv2.rectangle(img,(20,20), (750,60), (b,g,r), -1)
+
+        #Creating text string to display( Color name and RGB values )
+        text = getColorName(r,g,b) + ' R='+ str(r) +  ' G='+ str(g) +  ' B='+ str(b)
+        
+        #cv2.putText(img,text,start,font(0-7),fontScale,color,thickness,lineType )
+        cv2.putText(img, text,(50,50),2,0.8,(255,255,255),2,cv2.LINE_AA)
+
+        #For very light colours we will display text in black colour
+        if(r+g+b>=600):
+            cv2.putText(img, text,(50,50),2,0.8,(0,0,0),2,cv2.LINE_AA)
+            
+        clicked=False
+
+    #Break the loop when user hits 'esc' key    
+    if cv2.waitKey(20) & 0xFF ==27:
+        break
+    
+cv2.destroyAllWindows()
